6e9427ddc177a63d0151ec217b00cd2fc18a1001
Hong <hng@demystdata.com>
2016-07-09 18:23:59 +0800

    walk/jump/attack trigger zombie, to fix grounded issue

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BoxerControllerScript : MonoBehaviour {
+public class ZombieController : MonoBehaviour {
 
 	public float maxSpeed = 10f;
-	bool facingLeft = true;
+	bool facingLeft = false;
 
 	public Rigidbody2D character;
 	Animator anim;
-
-	bool grounded = false;
+	bool grounded;
+	//bool friendly = false;
 	public Transform groundCheck;
 	float groundRadius = 0.2f;
 	public LayerMask whatIsGround;
-	public float jumpForce = 700;
+	public float jumpForce = 600;
 
 	void Awake(){
 		character = GetComponent<Rigidbody2D>();
@@ -23,20 +23,20 @@ public class BoxerControllerScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
-		anim.SetBool ("Ground", grounded);
-
-		anim.SetFloat ("vSpeed", character.velocity.y);
+		anim.SetBool ("MGround", grounded);
+		Debug.Log (grounded);
+		anim.SetFloat ("vMSpeed", character.velocity.y);
 
 
 
 		float move = Input.GetAxis ("Horizontal");
-		anim.SetFloat ("Speed", Mathf.Abs (move));
+		anim.SetFloat ("MSpeed", Mathf.Abs (move));
 		character.velocity = new Vector2 (move * maxSpeed, character.velocity.y);
 
 		if (move > 0 && facingLeft)
@@ -44,14 +44,19 @@ public class BoxerControllerScript : MonoBehaviour {
 		else if (move < 0 && !facingLeft)
 			Flip ();
 	}
-
+		
 	void Update(){
 		// should not get input directly, should do it in input manager
 		if (grounded && Input.GetKeyDown (KeyCode.UpArrow)) {
-			anim.SetBool ("Ground", false);
+			anim.SetBool ("MGround", false);
 			character.AddForce(new Vector2(0,jumpForce));
-			
+
 		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Attack();
+		}
+
 	}
 
 	void Flip(){
@@ -60,4 +65,14 @@ public class BoxerControllerScript : MonoBehaviour {
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
+
+	void Attack(){
+		anim.SetBool("MAttack",true);
+
+	}
+	//this is used when event is triggered in ZombieAttack animation
+	void AttackDone(){
+		anim.SetBool ("MAttack", false);
+	}
+
 }
