25d915cfd6e74000063436f9e339c852ce0a5c88
nghei <ngheii@gmail.com>
2016-07-11 10:46:26 +0800

    Updated map

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -17,6 +17,10 @@ public class FollowPlayer : MonoBehaviour
 	void Update ()
 	{
 		// Set the position to the player's position with the offset.
-		transform.position = player.position + offset;
+		try
+		{
+			transform.position = player.position + offset;
+		}
+		catch (MissingReferenceException e) { }
 	}
 }
