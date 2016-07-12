1b28f82fbd13ace00e5ba9d7c1f6f640a661a43b
Virgil <Virgil@github.com>
2016-07-12 14:18:32 +0800

    zombieHit and zombieDead are ready!

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Zombie : BaseEnemy {
 
+
 	// Use this for initialization
 	void Start () {
 		base.Start ();
@@ -10,26 +11,16 @@ public class Zombie : BaseEnemy {
 		damage = 1;
 		hp = 500;
 		attackRange = 0.5f;
+		// Test for being attacked
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		base.Update ();
-
-		if (hp <= 0) {
-			Debug.Log ("Zombie Die");
-			gameObject.GetComponent<Animation> ().Play ("ZombieDead");
-			Destroy (gameObject);
-		}
 	}
 
-	public void Damage(int damage){
-		
-		Debug.Log("WTF Zombie is attacked");
-		hp -= damage;
-		gameObject.GetComponent<Animation> ().Play ("ZombieHit");
-		Debug.Log (gameObject);
-		Debug.Log ("hp: " + hp);
-		Debug.Log ("damage: " + damage);
+	void Damage(int damage){
+		base.Damage(damage);
 	}
+
 }
